ea792d83cd4c07b95f869b2ad62d3b5c344e77e7
Hui <jeffrey.x.hui@gmail.com>
2020-01-14 15:18:03 -0800

    finished writing specifications and MVP is hit

--- data/comments/commentsRoutes.js
+++ data/comments/commentsRoutes.js
@@ -7,10 +7,15 @@ router.get('/', (req, res) => {
   const postId = req.id_config.id;
   db.findPostComments(postId)
     .then( comments => {
-      res.status(200).json(comments);
+      if (!comments[0]) {
+        res.status(404).json({ errorMessage: `The post with the specified ID: ${postId} does not exist.` })
+      } else {
+        console.log(comments);
+        res.status(200).json(comments);
+      }
     })
     .catch( err => {
-      res.status(500).send(`Internal Server Error, could not find comments at postId ${postId}`)
+      res.status(500).send(`Internal Server Error, could not retrieve comments at postId ${postId}`)
     })
 })
 router.get('/:id', (req, res) => {
@@ -27,18 +32,42 @@ router.get('/:id', (req, res) => {
 })
 router.post('/', (req, res) => {
   const comment = req.body;
-  console.log(comment);
-  const id = req.id_config.id;
-  comment.post_id = id;
-
-  db.insertComment(comment)
+  const postId = req.id_config.id;
+  comment.post_id = postId;
+  const insertComment = db.insertComment(comment)
     .then( reso => {
-      res.status(200).json(reso);
+      res.status(200).json(comment);
     })
     .catch( err => {
       console.log(err);
-      res.status(500).send(`Internal server error 500: could not add comment at postId ${id}`);
+      res.status(500).send(`Internal server error 500: could not add comment at postId ${postId}`);
     })
+
+  if(!req.body.text) {
+    res.status(400).json({ errorMessage: "Please provide text for the comment." })
+  } else {
+    console.log('text included... searching for postId');
+    db.findById(postId)
+      .then(resolve => {
+        if (resolve[0]) {
+          console.log('postId found! ...attempting to insert comment', resolve)
+          insertComment(comment);
+        } else {
+          res.status(404).json({ message: `The post with the specified id: ${postId}does not exist.` })
+        }
+      })
+      .catch(err => {
+        // this never occurs
+      })
+
+  }
+
+
+
+  
+
+
+
 })
 
 module.exports = router;
--- data/posts/postsRoutes.js
+++ data/posts/postsRoutes.js
@@ -19,66 +19,97 @@ router.get('/', (req, res) => {
     })
     .catch( err => {
       console.log(err);
-      res.status(500).send('Internal Server Error 500: Could not retrieve posts');
+      res.status(500).json({ error: "Internal Server Error 500: Could not retrieve posts" });
     })
 });
 router.get('/:id', (req, res) => {
   const id = req.params.id;
   db.findById(id)
     .then( post => {
-      console.log(post);
-      res.status(200).json(post);
+      if(!post[0]) {
+        res.status(404).json({ errorMessage: `The post with the specified ID: ${id} does not exist` })
+      } else {
+        console.log(post);
+        res.status(200).json(post);
+      }
+
     })
     .catch( err => {
       console.log(err);
-      res.status(500).send(`Internal server error 500: c ould not retrieve post by id ${id}`);
+      res.status(500).send(`Internal server error 500: could not retrieve post by id ${id}`);
     })
 })
 // client post to /api/posts
 router.post('/', (req, res) => {
+  
   if(!req.body.title || !req.body.contents) {
     console.log(req.body);
     res.status(400).json({ errorMessage: "Please provide title and contents for the post." });
   } else {
+
     db.insert(req.body)
       .then( id => {
         console.log(id);
-        res.status(201).json(id);
+        res.status(201).json(req.body);
       })
       .catch( err => {
         console.log(err);
-        res.status(500).send('Internal Server Error 500: Could not add post');
+        res.status(500).send('Internal Server Error 500: Could not save post to database');
       });
   }
 
 });
+
 router.put('/:id', (req, res) => {
   const id = req.params.id;
   const post = req.body;
 
   console.log(id, post);
 
-  db.update(id, post)
-    .then( id => {
-      res.status(200).json(id);
-    })
-    .catch( err => {
-      console.log(err);
-      res.status(500).send('Internal server error 500: could not edit post');
-    })
+  if(!post.title || !post.contents) {
+    res.status(400).json({ errorMessage: `Please provide title and/or contents for the post.` })
+  } else {
+
+    db.findById(id)
+      .then( postAtId => {
+        if(!postAtId[0]) {
+          res.status(404).json({ errorMessage: `The post with the specified ID: ${id} does not exist.` })
+        } else {
+
+          db.update(id, post)
+            .then( id => {
+              res.status(200).json(post);
+            })
+            .catch( err => {
+              console.log(err);
+              res.status(500).send('Internal server error 500: could not edit post');
+            })
+        }
+      })
+  }
 })
+
 router.delete('/:id', (req, res) => {
   const id = req.params.id;
 
-  db.remove(id)
-    .then( deleted => {
-      console.log(deleted);
-      res.status(200).json(deleted);
+  db.findById(id)
+    .then( post => {
+      if(!post[0]) {
+        res.status(404).json({ errorMessage: `The post with the specified ID: ${id} does not exist.` })
+      } else {
+        db.remove(id)
+          .then( deleted => {
+            console.log(deleted);
+            res.status(200).json(post);
+          })
+          .catch( err => {
+            console.log(err);
+            res.status(500).send('Internal server error 500: could not delete resource')
+          })
+      }
     })
-    .catch( err => {
-      console.log(err);
-      res.status(500).send('Internal server error 500: could not delete resource')
-    })
+
+
 })
 
 module.exports = router;
