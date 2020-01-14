a672ae53a82d5eb151994a4841e1c068a7db1564
Hui <jeffrey.x.hui@gmail.com>
2020-01-14 13:18:52 -0800

    wrote out all the functionality, comments post currently broken

--- data/comments/commentsRoutes.js
+++ data/comments/commentsRoutes.js
@@ -4,18 +4,40 @@ const router = express.Router();
 const db = require('../db');
 
 router.get('/', (req, res) => {
-  const id = req.id_config.id;
-  console.log(id);
+  const postId = req.id_config.id;
+  db.findPostComments(postId)
+    .then( comments => {
+      res.status(200).json(comments);
+    })
+    .catch( err => {
+      res.status(500).send(`Internal Server Error, could not find comments at postId ${postId}`)
+    })
+})
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  
   db.findCommentById(id)
     .then( comment => {
       res.status(200).json(comment);
     })
     .catch( err => {
-      res.status(500).send(`Internal Server Error, could not find comment at id ${id}`)
+      console.log(err);
+      res.status(500).send(`Internal Server Error 500, could not find comment by id ${id}`)
     })
+})
+router.post('/', (req, res) => {
+  const post = req.body;
+  console.log(post);
+  const id = req.id_config.id;
 
-  
-
+  db.insertComment(post)
+    .then( reso => {
+      res.status(200).json(reso);
+    })
+    .catch( err => {
+      console.log(err);
+      res.status(500).send(`Internal server error 500: could not add comment at postId ${id}`);
+    })
 })
 
 module.exports = router;
--- data/posts/postsRoutes.js
+++ data/posts/postsRoutes.js
@@ -5,7 +5,6 @@ const db = require('../db');
 const comments = require('../comments/commentsRoutes');
 
 router.use('/:id/comments', (req, res, next) => {
-  console.log('hi');
   req.id_config = {
     id: req.params.id
   }
@@ -23,6 +22,18 @@ router.get('/', (req, res) => {
       res.status(500).send('Internal Server Error 500: Could not retrieve posts');
     })
 });
+router.get('/:id', (req, res) => {
+  const id = req.params.id;
+  db.findById(id)
+    .then( post => {
+      console.log(post);
+      res.status(200).json(post);
+    })
+    .catch( err => {
+      console.log(err);
+      res.status(500).send(`Internal server error 500: c ould not retrieve post by id ${id}`);
+    })
+})
 router.post('/', (req, res) => {
   db.insert(req.body)
     .then( id => {
@@ -34,5 +45,33 @@ router.post('/', (req, res) => {
       res.status(500).send('Internal Server Error 500: Could not add post');
     });
 });
+router.put('/:id', (req, res) => {
+  const id = req.params.id;
+  const post = req.body;
+
+  console.log(id, post);
+
+  db.update(id, post)
+    .then( id => {
+      res.status(200).json(id);
+    })
+    .catch( err => {
+      console.log(err);
+      res.status(500).send('Internal server error 500: could not edit post');
+    })
+})
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+
+  db.remove(id)
+    .then( deleted => {
+      console.log(deleted);
+      res.status(200).json(deleted);
+    })
+    .catch( err => {
+      console.log(err);
+      res.status(500).send('Internal server error 500: could not delete resource')
+    })
+})
 
 module.exports = router;
